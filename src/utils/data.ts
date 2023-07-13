af4624c5431c6db61eb3100f4b98cf82ae94f449
Senul <hardlove.mrplus@gmail.com>
2023-07-13 21:19:02 +0530

    data file added

---
+++ src/utils/data.ts
@@ -0,0 +1,57 @@
+/**
+ * List of all the pages
+ */
+export const pages = [
+  "",
+  "api",
+  "components",
+  "dashboard",
+  "email",
+  "info",
+  "users",
+  "_app",
+  "_document",
+  "about-us",
+  "contact",
+  "index",
+  "login",
+  "new",
+  "register",
+  "created-events",
+  "rewards",
+  "settings",
+];
+
+/**
+ * KeyWords that the users should not be able to use in
+ * feilds that are used to uniqly identify the user
+ */
+export const keywords = [
+  // Concerned words
+  "admin",
+  "information",
+  "state",
+  "validate",
+  "validation",
+  "support",
+  "contact",
+  "hotline",
+  "remove",
+  "delete",
+  "add",
+  "download",
+  "downloads",
+  "upload",
+  "uploads",
+  "verify",
+  "reserve",
+  "pages",
+  "page",
+  "api",
+  "blocked",
+  "_app",
+  "test",
+
+  // NEXTjs pages
+  ...pages,
+];
