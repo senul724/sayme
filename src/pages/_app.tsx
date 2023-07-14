279793e18b2482acfd6e50140ea7c4fa91e35b76
Senul <hardlove.mrplus@gmail.com>
2023-07-14 14:44:17 +0530

    react hot toast added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,6 +11,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Toaster position="top-left" />
       <Component {...pageProps} />
     </SessionProvider>
   );
