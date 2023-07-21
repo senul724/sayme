0decc34b18aef971cdb0dae011a39133d23c8877
Senul <hardlove.mrplus@gmail.com>
2023-07-21 12:31:18 +0530

    middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  console.log("route: ", request.nextUrl.pathname);
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+  const path = request.nextUrl.pathname;
+
+  if (path.startsWith("/") && token?.event && path !== "/dashboard") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (path.startsWith("/dashboard") && !token?.event && path !== "/") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/dashboard"],
+};
